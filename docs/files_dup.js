494d4792bf7aa27c422d50929a1b882009a21842
vololovololov-elec <85225475+vololovololov-elec@users.noreply.github.com>
2021-07-08 18:18:07 +0200

    Delete docs directory

--- docs/files_dup.js
+++
@@ -1,7 +0,0 @@
-var files_dup =
-[
-    [ "seq.vhdl", "seq_8vhdl.html", [
-      [ "myIP", "classmy_i_p.html", "classmy_i_p" ],
-      [ "myIP.arch", "classmy_i_p_1_1arch.html", "classmy_i_p_1_1arch" ]
-    ] ]
-];
