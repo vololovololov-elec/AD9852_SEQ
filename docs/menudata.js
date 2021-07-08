81a43ff0b0d902563d418538e7bb7b3ee059b481
vololovololov-elec <85225475+vololovololov-elec@users.noreply.github.com>
2021-07-08 11:17:21 +0200

    Add files via upload

--- docs/menudata.js
+++ docs/menudata.js
@@ -28,6 +28,8 @@ var menudata={children:[
 {text:"Design Unit List",url:"annotated.html",children:[
 {text:"Design Unit List",url:"annotated.html"},
 {text:"Design Units",url:"classes.html"},
+{text:"Design Unit Hierarchy",url:"hierarchy.html"},
 {text:"Design Unit Members",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
-{text:"Functions/Procedures/Processes",url:"functions_func.html"}]}]}]}
+{text:"Functions/Procedures/Processes",url:"functions_func.html"},
+{text:"Variables",url:"functions_vars.html"}]}]}]}
